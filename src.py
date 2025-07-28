18703db16a22d2b485bcac6cca0025518220b973
oxo970311 <148974384+oxo970311@users.noreply.github.com>
2025-07-28 16:07:30 +0900

    pepe_man
    pepe_man

--- src.py
+++ src.py
@@ -2,8 +2,24 @@ import cv2
 print("OpenCV version:", cv2.__version__)
 
 path = "C:/Users/oxo97/Downloads/like_lenna.png"
+path2 = "C:/Users/oxo97/Downloads/pepe.png"
 
-img = cv2.imread(path)
-cv2.imshow('lenna',img)
+lenna = cv2.imread(path)
+pepe = cv2.imread(path2)
+
+print(lenna.shape); print(pepe.shape)
+
+r_lenna = cv2.resize(lenna, (200,200))
+r_pepe = cv2.resize(pepe,(r_lenna.shape[0],r_lenna.shape[1]))
+
+
+print(r_lenna.shape); print(r_pepe.shape)
+
+alpha = 0.6
+beta = 0.9
+
+pepe_man = cv2.addWeighted(r_lenna, alpha, r_pepe, beta, 0)
+cv2.imshow("pepe_man", pepe_man)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
